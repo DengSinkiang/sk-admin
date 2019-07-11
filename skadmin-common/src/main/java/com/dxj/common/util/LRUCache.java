fdf6e589ad92932ff9e039a8bfcfcb6ba1de4b17
dxj <dengsinkiang@gmail.com>
2019-07-11 18:50:29 +0800

    dxj

---
+++ skadmin-common/src/main/java/com/dxj/common/util/LRUCache.java
@@ -0,0 +1,30 @@
+package com.dxj.common.util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * LRU 算法
+ * @Author: dxj
+ * @Date: 2019-07-11 18:49
+ */
+class LRUCache<K, V> extends LinkedHashMap<K, V> {
+    private final int CACHE_SIZE;
+
+    /**
+     * 传递进来最多能缓存多少数据
+     *
+     * @param cacheSize 缓存大小
+     */
+    public LRUCache(int cacheSize) {
+        // true 表示让 linkedHashMap 按照访问顺序来进行排序，最近访问的放在头部，最老访问的放在尾部。
+        super((int) Math.ceil(cacheSize / 0.75) + 1, 0.75f, true);
+        CACHE_SIZE = cacheSize;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        // 当 map中的数据量大于指定的缓存个数的时候，就自动删除最老的数据。
+        return size() > CACHE_SIZE;
+    }
+}
