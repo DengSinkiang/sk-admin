781f835114ac85c055fdd5fff6cafc607426fe83
DengSinkiang <dengsinkiang@gmail.com>
2019-07-16 22:36:11 +0800

    增加rabbitmq demo

---
+++ skadmin-rabbitmq-demo/src/main/java/com/dxj/rabbitmq/fanout/Recv.java
@@ -0,0 +1,44 @@
+package com.dxj.rabbitmq.fanout;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import com.dxj.rabbitmq.util.ConnectionUtil;
+
+//消费者1
+public class Recv {
+    private final static String QUEUE_NAME = "fanout_exchange_queue_1";
+
+    private final static String EXCHANGE_NAME = "fanout_exchange_test";
+
+    public static void main(String[] argv) throws Exception {
+        // 获取到连接
+        Connection connection = ConnectionUtil.getConnection();
+        // 获取通道
+        Channel channel = connection.createChannel();
+        // 声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        // 绑定队列到交换机
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, "");
+
+        // 定义队列的消费者
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            // 获取消息，并且处理，这个方法类似事件监听，如果有消息的时候，会被自动调用
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties,
+                    byte[] body) throws IOException {
+                // body 即消息体
+                String msg = new String(body);
+                System.out.println(" [消费者1] received : " + msg + "!");
+            }
+        };
+        // 监听队列，自动返回完成
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+}
---
+++ skadmin-rabbitmq-demo/src/main/java/com/dxj/rabbitmq/work/Recv2.java
@@ -0,0 +1,42 @@
+package com.dxj.rabbitmq.work;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import com.dxj.rabbitmq.util.ConnectionUtil;
+
+//消费者2
+public class Recv2 {
+    private final static String QUEUE_NAME = "test_work_queue";
+
+    public static void main(String[] argv) throws Exception {
+        // 获取到连接
+        Connection connection = ConnectionUtil.getConnection();
+        // 获取通道
+        final Channel channel = connection.createChannel();
+        // 声明队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        // 设置每个消费者同时只能处理一条消息
+        channel.basicQos(1);
+        // 定义队列的消费者
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            // 获取消息，并且处理，这个方法类似事件监听，如果有消息的时候，会被自动调用
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties,
+                    byte[] body) throws IOException {
+                // body 即消息体
+                String msg = new String(body);
+                System.out.println(" [消费者2] received : " + msg + "!");
+                // 手动ACK
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        };
+        // 监听队列。
+        channel.basicConsume(QUEUE_NAME, false, consumer);
+    }
+}
